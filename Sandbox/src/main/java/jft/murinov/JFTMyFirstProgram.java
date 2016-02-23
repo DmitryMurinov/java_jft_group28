bc32117921f168aa339dc2a28469fc7a95cb050b
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2016-02-23 16:43:09 +0300

    Изменены настройки форматирования и форматирование

--- Sandbox/src/main/java/jft/murinov/JFTMyFirstProgram.java
+++ Sandbox/src/main/java/jft/murinov/JFTMyFirstProgram.java
@@ -2,8 +2,8 @@ package jft.murinov;
 
 public class JFTMyFirstProgram {
 
-   public static void main(String[] args){
-	System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 
 }
