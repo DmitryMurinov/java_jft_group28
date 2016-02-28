1ab7e1175fe1080dbb268bfd10fdb2d7276ec387
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2016-02-28 19:31:36 +0300

    Сделал Group modify test

--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/appmanager/GroupHelper.java
@@ -38,4 +38,14 @@ public class GroupHelper extends HelperBase{
     public void selectGroup() {
         click(By.name("selected[]"));
     }
+
+    public void initGroupModify() {
+        click(By.name("edit"));
+
+
+    }
+
+    public void submitGroupModify() {
+        click(By.name("update"));
+    }
 }
---
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/GroupModifyTests.java
@@ -0,0 +1,20 @@
+package jft.murinov.addressbook.tests;
+
+import jft.murinov.addressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Dima on 28.02.2016.
+ */
+public class GroupModifyTests extends TestBase{
+
+    @Test
+    public void testGroupModify(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModify();
+        app.getGroupHelper().fillGroupCreationForm(new GroupData("test1mod", "test2mod", "test3mod"));
+        app.getGroupHelper().submitGroupModify();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
