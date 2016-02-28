64b00efea19a217fab593afa1aedbf57681130bc
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2016-02-28 10:56:46 +0300

    Сделал рефакторинг добавления контакта

--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/ContactCreationTests.java
@@ -1,21 +1,15 @@
 package jft.murinov.addressbook;
 
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class ContactCreationTests {
     FirefoxDriver wd;
@@ -25,30 +19,30 @@ public class ContactCreationTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String userName, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(userName);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
     public void ContactCreationTests() {
         clickAddNew();
-        fillContactForm();
+        fillContactForm(new ContactData("FirstName", "MiddleName", "LastName", "Nickname", "Address string", "+74951234567", "+75551234567", "nickname@mailserver.ru"));
         submitContactForm();
-        waitForAutoRedirectToContactsList();
+        waitForAutoRedirectToContactsList("nickname@mailserver.ru");
     }
 
-    private void waitForAutoRedirectToContactsList() {
+    private void waitForAutoRedirectToContactsList(String firstEmail) {
         new WebDriverWait(wd, 60, 200).until(
-                ExpectedConditions.elementToBeClickable(By.linkText("nickname@mailserver.ru"))
+                ExpectedConditions.elementToBeClickable(By.linkText(firstEmail))
         );
     }
 
@@ -56,31 +50,31 @@ public class ContactCreationTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillContactForm() {
+    private void fillContactForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("FirstName");
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
         wd.findElement(By.name("middlename")).click();
         wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys("MiddleName");
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("LastName");
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
         wd.findElement(By.name("nickname")).click();
         wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("Nickname");
+        wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("Address string");
+        wd.findElement(By.name("address")).sendKeys(contactData.getFirstAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("+74951234567");
+        wd.findElement(By.name("home")).sendKeys(contactData.getHomePhoneString());
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("+75551234567");
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhoneString());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("nickname@mailserver.ru");
+        wd.findElement(By.name("email")).sendKeys(contactData.getFirstEmail());
     }
 
     private void clickAddNew() {
---
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package jft.murinov.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickname;
+    private final String firstAddress;
+    private final String homePhoneString;
+    private final String mobilePhoneString;
+    private final String firstEmail;
+
+    public ContactData(String firstName, String middleName, String lastName, String nickname, String firstAddress, String homePhoneString, String mobilePhoneString, String firstEmail) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.firstAddress = firstAddress;
+        this.homePhoneString = homePhoneString;
+        this.mobilePhoneString = mobilePhoneString;
+        this.firstEmail = firstEmail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getFirstAddress() {
+        return firstAddress;
+    }
+
+    public String getHomePhoneString() {
+        return homePhoneString;
+    }
+
+    public String getMobilePhoneString() {
+        return mobilePhoneString;
+    }
+
+    public String getFirstEmail() {
+        return firstEmail;
+    }
+}
