00664f0ee03ae3ed1f8ed2c474f01e7c447cf004
DmitryMurinov <d.murinov@naproc.com>
2016-03-14 15:51:56 +0300

    л4м10 сделал
    Реализовал сравнение с сортировкой для создания и модификации групп и
    контактов

--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/model/GroupData.java
@@ -17,7 +17,7 @@ public class GroupData {
 
 
     public GroupData(String groupName, String groupHeader, String groupFooter) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.groupName = groupName;
         this.groupHeader = groupHeader;
         this.groupFooter = groupFooter;
@@ -58,16 +58,13 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (id != groupData.id) return false;
         return groupName != null ? groupName.equals(groupData.groupName) : groupData.groupName == null;
 
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (groupName != null ? groupName.hashCode() : 0);
-        return result;
+        return groupName != null ? groupName.hashCode() : 0;
     }
 
 }
