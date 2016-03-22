a326dd40c0512a8b43b032729309bce48cc8e849
DmitryMurinov <d.murinov@naproc.com>
2016-03-22 15:59:27 +0300

    Обновления из офиса, мелкие

--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import jft.murinov.addressbook.model.GroupData;
 import jft.murinov.addressbook.model.Groups;
 import org.testng.annotations.Test;
 
+import static com.google.common.base.Predicates.equalTo;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
 
