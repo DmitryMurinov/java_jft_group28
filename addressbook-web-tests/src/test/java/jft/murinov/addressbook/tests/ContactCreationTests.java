56cc34a98953a06d11a96848a01be737cb3d3f19
DmitryMurinov <murinov.dmitry.acc@gmail.com>
2016-03-25 11:42:12 +0300

    л6м4 добавил провайдеры данных для генерации групп и контактов

--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/ContactCreationTests.java
@@ -2,15 +2,35 @@ package jft.murinov.addressbook.tests;
 
 import jft.murinov.addressbook.model.ContactData;
 import jft.murinov.addressbook.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        File photo = new File("src/test/resources/people_to_remember.jpg");
+        list.add(new Object[] {new ContactData()
+                .withFirstName("FirstName1").withMiddleName("MiddleName").withLastName("LastName1").withNickname("Nickname").withFirstAddress("Address string")
+                .withHomePhone("+74951234567").withMobilePhone("+75551234567").withFirstEmail("nickname@mailserver.ru").withGroup("test1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData()
+                .withFirstName("FirstName2").withMiddleName("MiddleName").withLastName("LastName2").withNickname("Nickname").withFirstAddress("Address string")
+                .withHomePhone("+74951234567").withMobilePhone("+75551234567").withFirstEmail("nickname@mailserver.ru").withGroup("test1").withPhoto(photo)});
+        list.add(new Object[] {new ContactData()
+                .withFirstName("FirstName3").withMiddleName("MiddleName").withLastName("LastName3").withNickname("Nickname").withFirstAddress("Address string")
+                .withHomePhone("+74951234567").withMobilePhone("+75551234567").withFirstEmail("nickname@mailserver.ru").withGroup("test1").withPhoto(photo)});
+        return list.iterator();
+    }
+  /*
     @Test(enabled = true)
     public void testContactCreation() {
         Contacts before = app.contact().all();
@@ -22,15 +42,11 @@ public class ContactCreationTests extends TestBase{
         Contacts after = app.contact().all();
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-    }
+    }*/
 
-    @Test(enabled = true)
-    public void testContactCreationPhoto() {
+    @Test(enabled = true, dataProvider = "validContacts")
+    public void testContactCreationPhoto(ContactData contact) {
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/people_to_remember.jpg");
-        ContactData contact = new ContactData()
-                .withFirstName("FirstName2Photo").withMiddleName("MiddleName").withLastName("LastName").withNickname("Nickname").withFirstAddress("Address string")
-                .withHomePhone("+74951234567").withMobilePhone("+75551234567").withFirstEmail("nickname@mailserver.ru").withGroup("test1").withPhoto(photo);
         app.contact().create(contact, true);
         assertThat(app.contact().count(), equalTo(before.size() + 1));
         Contacts after = app.contact().all();
--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/tests/GroupCreationTests.java
@@ -2,23 +2,36 @@ package jft.murinov.addressbook.tests;
 
 import jft.murinov.addressbook.model.GroupData;
 import jft.murinov.addressbook.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase{
 
-    @Test
-    public void testGroupCreation() {
-        app.goTo().GroupPage();
-        Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test2");
-        app.group().create(group);
-        assertThat(app.group().count(), equalTo(before.size() + 1));
-        Groups after = app.group().all();
-        assertThat(after, equalTo(
-                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1").withFooter("footer1").withHeader("header1")});
+        list.add(new Object[] {new GroupData().withName("test2").withFooter("footer2").withHeader("header2")});
+        list.add(new Object[] {new GroupData().withName("test3").withFooter("footer3").withHeader("header3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
+                    app.goTo().GroupPage();
+                    Groups before = app.group().all();
+                    app.group().create(group);
+                    assertThat(app.group().count(), equalTo(before.size() + 1));
+                    Groups after = app.group().all();
+                    assertThat(after, equalTo(
+                            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
     @Test
