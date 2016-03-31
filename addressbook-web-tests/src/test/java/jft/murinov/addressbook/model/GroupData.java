2957b6d8e96db7aa4c30fc11055a14bb514d7274
DmitryMurinov <murinov.dmitry.acc@gmail.com>
2016-03-31 16:18:15 +0300

    l7m2 Hybernate Connection к БД MySQL сделал для групп

--- addressbook-web-tests/src/test/java/jft/murinov/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/jft/murinov/addressbook/model/GroupData.java
@@ -3,17 +3,35 @@ package jft.murinov.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @XStreamAlias("Group")
+@Entity
+@Table(name ="group_list")
 public class GroupData {
 
     @XStreamOmitField
+    @Id
+    @Column(name = "group_id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column(name = "group_name")
     private String groupName;
+
     @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
     private String groupHeader;
+
     @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
     private String groupFooter;
 
     public GroupData withId(int id) {
