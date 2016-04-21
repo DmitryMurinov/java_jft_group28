db6c36c3a25ce604d97c6120476ef7f0f0e80a37
DmitryMurinov <murinov.dmitry.acc@gmail.com>
2016-04-21 17:43:55 +0300

    Задание 20 выполнил.

--- mantis-tests/src/test/java/learn/mantis/appmanager/AdministrationHelper.java
+++ mantis-tests/src/test/java/learn/mantis/appmanager/AdministrationHelper.java
@@ -15,4 +15,19 @@ public class AdministrationHelper extends HelperBase{
     public void openUsersMenu() {
         click(By.xpath("/html/body/div[2]/p/span[1]/a"));
     }
+
+    public void openUser(String username) {
+        click(By.linkText(username));
+    }
+
+    public void resetUserPassword() {
+        click(By.cssSelector("input[value='Reset Password']"));
+    }
+
+    public void changePasswordFromLink(String passwordChangeLink, String newpass) {
+        wd.get(passwordChangeLink);
+        typeInfoBox(By.name("password"), newpass);
+        typeInfoBox(By.name("password_confirm"), newpass);
+        click(By.cssSelector("input[value='Update User']"));
+    }
 }
--- mantis-tests/src/test/java/learn/mantis/tests/AdministrationTests.java
+++ mantis-tests/src/test/java/learn/mantis/tests/AdministrationTests.java
@@ -1,26 +1,51 @@
 package learn.mantis.tests;
 
+import learn.mantis.Model.MailMessage;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+
+import java.util.List;
 
 import static learn.mantis.tests.TestBase.app;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Created by d.murinov on 21.04.2016.
  */
 public class AdministrationTests extends TestBase{
 
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
     @Test
     public void changeUserPassword(){
+        String user = "user1";
+        String newPassword = "pass" + System.currentTimeMillis();
         app.registration().login("administrator", "root");
-//        app.administration().openUsersMenu();
+        app.administration().openUsersMenu();
+        app.administration().openUser(user);
+        app.administration().resetUserPassword();
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+        String passwordChangeLink = findPasswordChangeLink(mailMessages, "user1@localhost.localdomain");
+        app.administration().changePasswordFromLink(passwordChangeLink, newPassword);
+        app.registration().login(user, newPassword);
+        assertTrue(app.getWebDriver().findElement(By.linkText("Logout")) != null);
+    }
 
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private String findPasswordChangeLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage =  mailMessages.stream().filter((m) -> m.to.equals(email)).findAny().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
 
-
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
     }
 }
